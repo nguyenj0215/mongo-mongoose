4a105791c700db46d33fc2323d322d737095b95b
Nguyenj0215 <Nguyenj0215@gmail.com>
2019-10-21 21:03:16 -0700

    restrucutred for handlebars and removed routes folder

--- server.js
+++ server.js
@@ -1,27 +1,33 @@
 var express = require("express");
 var mongoose = require("mongoose");
-
-var PORT = 8080;
-
-// Require all models
-//var db = require("./models");
+var exphbs = require("express-handlebars");
+var axios = require("axios");
+var cheerio = require("cheerio");
 
 // Initialize Express
 var app = express();
 
+//Define port 8080
+var PORT = process.env.PORT || 8080;
+
+// Initialize handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Make public a static folder
 app.use(express.static("public"));
 
-require("./routes/html-routes.js")(app);
-//require("./routes/user-api-routes.js")(app);
-
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadLines";
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true , useUnifiedTopology: true });
 
-// Start the server
+// Start the server listener
 app.listen(PORT, function() {
     console.log("🌎 App running on port " + PORT + "!");
   });
+
+  // "/" Route
